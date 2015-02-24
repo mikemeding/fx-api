be5340ee16cc1f29a8e95c499ff875b052cd1fcf
mikemeding <mikeymeding@gmail.com>
2015-02-24 13:39:02 -0500

    added sql backup files

---
+++ fx-rest/data/users.sql
@@ -0,0 +1,21 @@
+REATE TABLE fx_users (
+    list INT,
+    list_active INT,
+    list_can_edit_pages INT,
+    list_email VARCHAR(31) CHARACTER SET utf8,
+    list_fullname VARCHAR(27) CHARACTER SET utf8,
+    list_id INT,
+    list_username VARCHAR(10) CHARACTER SET utf8
+);
+INSERT INTO fx_users VALUES (NULL,1,0 ,'NULL','Default User / Do Not Erase',3,'DEFAULT');
+INSERT INTO fx_users VALUES (NULL,1,1   ,'meding@yahoo.com','Uwe Meding',3,'UWE');
+INSERT INTO fx_users VALUES (NULL,1,1,'unclaimedfunds@ufr.name','Johel Contreras',4,'JOHEL');
+INSERT INTO fx_users VALUES (NULL,1,1,'NULL','Sary Santillana',4322858,'SARI');
+INSERT INTO fx_users VALUES (NULL,1,0 ,'NULL','Cecy Cristales',4322855,'CECY');
+INSERT INTO fx_users VALUES (NULL,1,0,'NULL','Carolina Cristales',4322857,'CAROLINA');
+INSERT INTO fx_users VALUES (NULL,0,0 ,'NULL','Carlos Coreas',4323375,'CARLOS C');
+INSERT INTO fx_users VALUES (NULL,1,0,'NULL','Hilda Contreras',4323380,'HILDA');
+INSERT INTO fx_users VALUES (NULL,0,0 ,'NULL','Pollet',14772423,'POLLET');
+INSERT INTO fx_users VALUES (NULL,0,1 ,'NULL','Juan Lopez',14792520,'JUAN LOPEZ');
+INSERT INTO fx_users VALUES (NULL,1,1  ,'customerservice@unclaimedfr.com','Polette M',21617252,'POLETTE');
+INSERT INTO fx_users VALUES (NULL,1,0 ,'NULL','Dulce Alonso',26692018,'DULCE');
